1bda16ad0f6583f269a28f685c8b6d272d56daeb
Dameredet <alexandradziwinska@gmail.com>
2022-12-21 13:27:20 +0100

    Sry, nie ta wersja

---
+++ ZTP/Projekt-KCK/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Projekt_KCK;
+using Projekt_KCK.Controllers;
+using Projekt_KCK.Views;
+
+
+namespace Projekt_KCK
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            Console.SetWindowSize(237, 63);
+            Console.CursorVisible = false;
+
+            var GraphicsManager = GraphicMode.GetInstance();
+            GraphicsManager.TurnOnConsoleMode();
+
+            
+            
+            var menuController = MenuController.GetInstance();
+            menuController.LoadLevelNames();
+            menuController.Menu();
+            
+        }
+    }
+}
---
+++ ZTP/Projekt-KCK/Views/LoadingView.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projekt_KCK.Views
+{
+    interface ILoadingView
+    {
+        void Load();
+    }
+    class LoadingView : ILoadingView
+    {
+        public void Load()
+        {
+            Console.Clear();
+
+            Console.SetCursorPosition(Console.CursorLeft, Console.WindowHeight / 2);
+            string textToEnter = "█▒▒▒▒▒▒▒▒▒ 10%";
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (textToEnter.Length / 2)) + "}", textToEnter));
+            System.Threading.Thread.Sleep(300);
+            Console.SetCursorPosition(Console.CursorLeft, Console.WindowHeight / 2);
+            textToEnter = "██▒▒▒▒▒▒▒▒ 20%";
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (textToEnter.Length / 2)) + "}", textToEnter));
+            System.Threading.Thread.Sleep(300);
+            Console.SetCursorPosition(Console.CursorLeft, Console.WindowHeight / 2);
+            textToEnter = "███▒▒▒▒▒▒▒ 30%";
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (textToEnter.Length / 2)) + "}", textToEnter));
+            System.Threading.Thread.Sleep(300);
+            Console.SetCursorPosition(Console.CursorLeft, Console.WindowHeight / 2);
+            textToEnter = "████▒▒▒▒▒▒ 40%";
+            Console.SetCursorPosition(Console.CursorLeft, Console.WindowHeight / 2);
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (textToEnter.Length / 2)) + "}", textToEnter));
+            System.Threading.Thread.Sleep(300);
+            textToEnter = "█████▒▒▒▒▒ 50%";
+            Console.SetCursorPosition(Console.CursorLeft, Console.WindowHeight / 2);
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (textToEnter.Length / 2)) + "}", textToEnter));
+            System.Threading.Thread.Sleep(600);
+            textToEnter = "██████▒▒▒▒ 60%";
+            Console.SetCursorPosition(Console.CursorLeft, Console.WindowHeight / 2);
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (textToEnter.Length / 2)) + "}", textToEnter));
+            System.Threading.Thread.Sleep(200);
+            textToEnter = "███████▒▒▒ 70%";
+            Console.SetCursorPosition(Console.CursorLeft, Console.WindowHeight / 2);
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (textToEnter.Length / 2)) + "}", textToEnter));
+            System.Threading.Thread.Sleep(400);
+            textToEnter = "████████▒▒ 80%";
+            Console.SetCursorPosition(Console.CursorLeft, Console.WindowHeight / 2);
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (textToEnter.Length / 2)) + "}", textToEnter));
+            System.Threading.Thread.Sleep(300);
+            textToEnter = "█████████▒ 90%";
+            Console.SetCursorPosition(Console.CursorLeft, Console.WindowHeight / 2);
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (textToEnter.Length / 2)) + "}", textToEnter));
+            System.Threading.Thread.Sleep(1300);
+        }
+    }
+
+    class GraphicLoadingView: ILoadingView
+    {
+        public void Load()
+        {
+
+        }
+    }
+}
